6b20684591730475971f98cd610041c0aeaad96d
pyeres <py1@uw.edu>
2016-04-22 16:27:52 -0400

    add script to move csv to s3

---
+++ data_move.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# get taxi data and send it to s3
+# to make this executable, type 'chmod +x name_of_file.sh'
+# to run, type 'type sh ./name_of_file.sh'
+
+for year in {2010..2015}
+do
+  for month in {1..12}
+  do
+    echo wget https://storage.googleapis.com/tlc-trip-data/$year/yellow_tripdata_$year-$month.csv
+    echo aws s3 cp yellow_tripdata_$year-$month.csv s3://taxi-news
+    echo rm yellow_tripdata_$year-$month.csv
+  done
+done
+echo complete
